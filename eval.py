4c8b484e3b6cbcf5f5ac85ccd3e62b95cd5d12f3
amelia <ameliacherian@gmail.com>
2015-07-27 13:42:23 -0400

    test

--- eval.py
+++ eval.py
@@ -1,4 +1,20 @@
-def main (rankList, qrels):
+import sys
+
+
+def main (argv, rankList):
+    files = argv[1:]
+
+    qrel = {}
+    for x in files:
+        with open (x, 'r') as f:
+            for lines in f:
+                info = lines.split(' ')
+                if info[0] in qrel:
+                    qrel[info[0]][info[2]] = (info[3]).replace('\n', '')
+                else:
+                    qrel[info[0]] = {info[2]:info[3].replace('\n','')}
+    print (qrel)
+    
     numC = len(qrels)
     for x in rankList:
         if x in qrels:
@@ -9,3 +25,5 @@ def main (rankList, qrels):
     fN+=len(qrels)
     print (cP, fP, fN)
     
+if __name__ == '__main__':
+    main(sys.argv, [0,0])
