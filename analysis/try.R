edd19ca8481431a7c89f2af6d7f60fa58c7d1f14
amelia <ameliacherian@gmail.com>
2015-08-19 13:05:18 -0400

    more efficient

--- analysis/try.R
+++ analysis/try.R
@@ -1,20 +1,22 @@
-graphing <-function(l){
-mydata <- read.csv("~/Documents/search/iprec.txt", header = TRUE, sep = " ")
-names(mydata) = c('topicnumber', 'recall', 'precision')
-
-sample <- as.numeric(mydata$topicnumber)
-hello <- c()
-for (num in l){
-  hello <- c(hello, subset(mydata, topicnumber==num))}
-
-cl <- rainbow(length(l))
-
-plot (hello[2]$recall, hello[3]$precision, main = "i-Prec", xlab = "Recall", ylab = "Precision", type = 'o', col = cl[1], xlim = c(0.0, 1.0), ylim = c(0.0, 1.0))
-
-for (counter in 4:length(hello)){
-    lines(hello[counter+1]$recall, hello[counter+2]$precision, col=cl[(counter+2)/3], type = 'o')}
-
-legend("topleft", legend = l, col=cl[1:length(l)], pch=1) 
-
+graphing <- function(){#l) {
+  mydata <- read.csv("~/Documents/search/iprec.txt", header = FALSE, sep = " ")
+  names(mydata) = c('topicnumber', 'recall', 'precision')
+  l = unique(mydata$topicnum)
+  
+  plot(NULL, xlim = c(0.0, 1.0), ylim = c(0.0, 1.0), main = "i-Prec", xlab = "Recall", ylab = "Precision")
+  r<-c(0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
+  #p<-c()
+  counter = 0
+  
+  cl <- rainbow(length(l))
+  
+  for (num in l) {
+    counter = counter+1
+    p <- subset(mydata, topicnumber == num)$precision
+    print (subset(mydata, topicnumber == num))
+    lines(r, p,col = cl[(counter)], type = 'o')
+  }
+  
+  #legend("bottomright", legend = l, col = cl[1:length(l)], pch = 1)
+  
 }
-
