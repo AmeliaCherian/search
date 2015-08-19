0d2f153af91c68e61d4796aaed856a1797d3cc6a
amelia <ameliacherian@gmail.com>
2015-08-19 13:03:45 -0400

    both functions added

--- analysis/new.R
+++ analysis/new.R
@@ -1,3 +1,4 @@
 source("~/Documents/search/analysis/try.R")
-graphing(c('402', '335', '369', '436', '385', '390', '354', '420', '380', '401', '313'))
-
+source("~/Documents/search/analysis/another.R")
+graphing()#(c('402', '335', '369', '436', '385', '390', '354', '420', '380', '401', '313'))
+#evaluate("~/Documents/search/qrels/ten.LA", "~/Documents/search/ranks.txt")
